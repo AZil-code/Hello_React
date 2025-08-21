a8a28e89a26848138a9f76c2db104dc443d3b278
Aviel Zilberman <avilzilberman@gmail.com>
2025-08-21 14:16:24 +0300

    Added styling to AnimalsContainer

--- cmps/AnimalList.jsx
+++ cmps/AnimalList.jsx
@@ -2,17 +2,24 @@
 export function AnimalList({ animalInfos }) {
    const searchUrl = `https://www.google.com/search?q=`;
 
+   function calculateOpacity(count) {
+      let opacity = count * 0.001;
+      return opacity > 0.15 ? opacity : 0.15;
+   }
+
    return (
       <section className="animals-container">
          <h1>Rare Animals</h1>
          <table className="animals-table">
             <tbody>
-               {animalInfos.map((animal) => (
-                  <tr key={animal.type}>
-                     <td>{animal.type}</td>
-                     <td>{animal.count}</td>
+               {animalInfos.map(({ type, count }) => (
+                  <tr key={type}>
+                     <td>{type}</td>
+                     <td>{count}</td>
                      <td>
-                        <a href={searchUrl + animal.type}>Search</a>
+                        <a href={searchUrl + type} style={{ opacity: calculateOpacity(count) }}>
+                           Search
+                        </a>
                      </td>
                   </tr>
                ))}
