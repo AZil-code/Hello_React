bf6838e2f476c409836f40085f287532351ee36a
Aviel Zilberman <avilzilberman@gmail.com>
2025-08-21 19:02:03 +0300

    SeasonClock - addded dynamic date values

--- cmps/SeasonClock.jsx
+++ cmps/SeasonClock.jsx
@@ -1,20 +1,54 @@
 // import { useState } from 'react';
 
-const { useState } = React;
+const { useState, useEffect } = React;
+const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const monthNames = [
+   'January',
+   'February',
+   'March',
+   'April',
+   'May',
+   'June',
+   'July',
+   'August',
+   'September',
+   'October',
+   'November',
+   'December',
+];
+const seasonTransitions = [new Date(2000, 2, 20), new Date(2000, 5, 20), new Date(2000, 8, 22), new Date(2000, 11, 21)];
 
 export function SeasonClock() {
    const [isDark, setIsDark] = useState(false);
+   const [timer, setTimer] = useState(new Date());
+   let day, month, season;
+   useEffect(() => {
+      const intId = setInterval(() => setTimer(new Date()), 1000);
+
+      return () => clearInterval(intId);
+   });
 
    function onToggleDarkMode() {
       setIsDark(!isDark);
    }
 
+   function getSeason(date) {
+      const checkDate = new Date(2000, date.getMonth(), date.getDate());
+      if (checkDate < seasonTransitions[0] || checkDate >= seasonTransitions[3]) return 'Winter';
+      if (checkDate < seasonTransitions[1]) return 'Spring';
+      if (checkDate < seasonTransitions[2]) return 'Summer';
+      if (checkDate < seasonTransitions[3]) return 'Autumn';
+   }
+
+   day = dayNames[timer.getDay()];
+   month = monthNames[timer.getMonth()];
+   season = getSeason(timer);
    const isDarkClass = isDark ? 'dark' : '';
    return (
       <section className={`season-clock ${isDarkClass}`} onClick={onToggleDarkMode}>
-         <h2>December (Winter)</h2>
-         <img src="./assets/img/season-imgs/autumn.png"></img>
-         <h3>Saturady</h3>
+         <h2>{`${month} (${season})`}</h2>
+         <img src={`./assets/img/season-imgs/${season.toLowerCase()}.png`}></img>
+         <h3>{day}</h3>
       </section>
    );
 }
